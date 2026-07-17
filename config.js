4a20d72884309a428a5d2a71eed463dc38f81ceb
eligota93 <eligota93@gmail.com>
2026-07-17 22:47:53 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.KAMBOS_CONFIG = {
-  supabaseUrl: '',
-  supabaseAnonKey: '',
+  supabaseUrl: 'https://xjuerzvagxiwvjyacujm.supabase.co',
+  supabaseAnonKey: 'sb_publishable_HC_8XVAM4vRL2MILAqQcFA_f3xm22pP',
   whatsapp: '355694454008',
   seasonEnd: '2026-10-15'
 };
